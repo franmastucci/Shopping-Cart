110681d63bf49196bf01b47f534e93b0d717e9a6
franmastucci <franmastucci@gmail.com>
2021-05-19 01:30:33 -0300

    backdrop y sidedrawer funcionando

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllProducts, getProductById } = require('../controller/productControllers')
+
+//@desc GET all products from  db
+// @route GET /apir/products
+//@access Public
+
+router.get('/', getAllProducts);
+
+//@desc GET all products from  db
+// @route GET /apir/products
+//@access Public
+
+router.get('/:id', getProductById);
+
+module.exports = router;
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,9 @@
+import React, {} from 'react';
+import './CartScreen.css';
+
+const CartScreen = () => {
+    return ( <div className = "cartscreen" >Cart Screen </div>
+    )
+}
+
+export default CartScreen
